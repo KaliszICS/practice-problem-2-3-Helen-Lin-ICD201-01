43684c470a6097265f2566b70e51f1f35c65e7b2
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-11-05 22:40:53 +0000

    jungkook

--- PP2_3.py
+++ PP2_3.py
@@ -7,7 +7,7 @@
 
 def q1(): 
   #Write Assignment code here
-  word = input("Input a word: ")
+  word = input("In: ")
   if word[-1] == "y":
     print("-ies")
   elif word[-1] == "ey": 
@@ -19,7 +19,7 @@ def q1():
 
 def q2(): 
   #Write Assignment code here
-  num = input("Input an integer: ")
+  num = input("In: ")
   num = int(num)
   if num > 0:
     print(f'{num} is Positive')
@@ -28,14 +28,12 @@ def q2():
     print(f'{num} is Negative')
 
 def q3():
-  side1 = input("Input a number: ")
-  side1 = float(side1)
-  side2 = input("Input another number: ")
-  side2 = float(side2)
-  side3 = input("Input a third number: ")
-  side3 = float(side3)
+  side1 = float(input("Input a number: "))
+  side2 = float(input("Input another number: "))
+  side3 = float(input("Input a third number: "))
+  
 
-  if side1 == side2 == side3: 
+  if side1 == side2 and side1 == side3 and side2 == side3: 
     print("Equalateral")
   elif side1 == side2 or side1 == side3 or side2 == side3:
     print("Isosceles")
@@ -48,6 +46,6 @@ def q3():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
+#q1()
+#q2()
+#q3()
