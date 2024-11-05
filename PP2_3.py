5969d9dec36e750de92d2692aef21ca134b3bc5e
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-11-05 22:57:59 +0000

    jungkook

--- PP2_3.py
+++ PP2_3.py
@@ -8,39 +8,37 @@
 def q1(): 
   #Write Assignment code here
   word = input("In: ")
-  if word[-1] == "y":
-    print("-ies")
-  elif word[-1] == "ey": 
+  if word.endswith("ey"): 
     print("-eys")
-  elif word[-1] == "ife":
+  elif word.endswith("y"):
+    print("-ies")
+  elif word.endswith("ife"):
     print("-ives")
   else:
     print("-s")
 
 def q2(): 
   #Write Assignment code here
-  num = input("In: ")
-  num = int(num)
+  num = int(input("In: "))
   if num > 0:
-    print(f'{num} is Positive')
-
+    print(f'{num} is positive')
   elif num < 0:
-    print(f'{num} is Negative')
+    print(f'{num} is negative')
 
 def q3():
   side1 = float(input("Input a number: "))
-  side2 = float(input("Input another number: "))
-  side3 = float(input("Input a third number: "))
+  side2 = float(input("Input a number: "))
+  side3 = float(input("Input a number: "))
   
 
   if side1 == side2 and side1 == side3 and side2 == side3: 
-    print("Equalateral")
-  elif side1 == side2 or side1 == side3 or side2 == side3:
+    print("Equilateral")
+  elif side1 == side2 and side1 != side3 or side1 == side3 and side1 != side2 or side2 == side3 and side2 != side1:
     print("Isosceles")
-  elif side1 != side2 or side1 != side3 or side2 != side3:
+  elif (side1 + side2 < side3) or (side1 + side3 < side2) or (side2 + side3 < side1):
+    print("No Triangle")
+  elif side1 != side2 and side1 != side3 and side2 != side3:
     print("Scalene")
-  elif side1 + side2 < side3 or side1 + side3 < side2 or side3 + side2 < side1:
-    print("No triangle")
   
 
 #Do not alter the following code
